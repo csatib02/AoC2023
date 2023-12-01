f3b6ca559ab7457c96c116adea2f125a50d5dad8
csatib02 <csatib02@gmail.com>
2023-12-01 16:48:51 +0100

    Completed: First day.

--- Days/day_1/day_1.go
+++ Days/day_1/day_1.go
@@ -1,9 +1,103 @@
 package day_1
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-func main() {
-	fmt.Println("Hello World")
+type DigitNumbers struct {
+	numbers map[string]int
+}
+
+func NewDigitNumbers() *DigitNumbers {
+	return &DigitNumbers{
+		numbers: map[string]int{
+			"zero":  0,
+			"one":   1,
+			"two":   2,
+			"three": 3,
+			"four":  4,
+			"five":  5,
+			"six":   6,
+			"seven": 7,
+			"eight": 8,
+			"nine":  9,
+		},
+	}
+}
+
+func T_1(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		firstNumber, lastNumber := processLines(line)
+		sum += firstNumber*10 + lastNumber
+	}
+
+	return sum
+}
+
+func T_2(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		processedLine := replaceSpelledOutNumbers(line)
+		firstNumber, lastNumber := processLines(processedLine)
+		sum += firstNumber*10 + lastNumber
+	}
+
+	return sum
+}
+
+func replaceSpelledOutNumbers(line string) string {
+	digit := NewDigitNumbers()
+	var result []string
+	var currentNum string
+
+	for idx, char := range line {
+		currentNum += string(char)
+
+		if char >= '0' && char <= '9' {
+			result = append(result, string(char))
+			currentNum = ""
+		}
+
+		if shouldCheckForSuffix(idx, char) {
+			for word, number := range digit.numbers {
+				if strings.HasSuffix(currentNum, word) {
+					result = append(result, strconv.Itoa(number))
+					currentNum = string(char)
+					break
+				}
+			}
+		}
+	}
+	result = append(result, currentNum)
+
+	return strings.Join(result, "")
+}
+
+func shouldCheckForSuffix(idx int, char rune) bool {
+	return idx+1 >= 3 && strings.Contains("eorxnt", string(char))
+}
+
+func processLines(line string) (int, int) {
+	firstNumber := 0
+	lastNumber := 0
+
+	for _, char := range line {
+		if unicode.IsDigit(char) {
+			digit, _ := strconv.Atoi(string(char))
+			if firstNumber == 0 {
+				firstNumber = digit
+			} else {
+				lastNumber = digit
+			}
+		}
+	}
+
+	if lastNumber == 0 {
+		lastNumber = firstNumber
+	}
+
+	return firstNumber, lastNumber
 }
--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-	// "AoC/days/day_1"
+	"AoC/days/day_1"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	lines, err := getData("days/day_1/day_1_input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	result1 := day_1.T_1(lines)
+	result2 := day_1.T_2(lines)
+
+	fmt.Printf("Day 1.1: %v\n", result1)
+	fmt.Printf("Day 1.2: %v\n", result2)
+}
+
+func getData(filepath string) ([]string, error) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("file reading error %v", err)
+	}
+	lines := strings.Split(strings.ReplaceAll(string(data), "\r\n", "\n"), "\n")
+
+	return lines, nil
 }
